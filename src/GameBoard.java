841f908173189c47fa74f7a4748d26b4fb931b09
npaganini <npaganin@itba.edu.ar>
2020-08-13 19:33:21 -0300

    Refactored board so it's outside state.

---
+++ src/GameBoard.java
@@ -0,0 +1,126 @@
+import java.awt.*;
+import java.io.*;
+import java.util.*;
+import java.util.List;
+
+public class GameBoard {
+    // actual map being played:
+    private final List<List<Tile>> gameBoard;
+    // where the target tiles are:
+    private Set<Point> targetCellsPositions;
+    // actual amount of cells filled
+    private int currentTargetCellsCompleted;
+    // the player's position on the map:
+    private Point playerPosition;
+
+    public GameBoard(String configurationFilepath) throws IOException {
+        this.gameBoard = new ArrayList<>();
+        targetCellsPositions = new HashSet<>();
+        int row = 0;
+        int col = 0;
+
+        File configurationFile = new File(configurationFilepath);
+        BufferedReader br = new BufferedReader(new FileReader(configurationFile));
+        String line;
+        while ((line = br.readLine()) != null) {
+            gameBoard.add(new ArrayList<>());
+            for (Character character : line.toCharArray()) {
+                gameBoard.get(row).add(processTileFromChar(row, col, character));
+                col += 1;
+            }
+            row += 1;
+            col = 0;
+        }
+    }
+
+    public GameBoard(List<List<Tile>> gameBoard) {
+        this.gameBoard = gameBoard;
+    }
+
+    public List<List<Tile>> getGameBoard() {
+        return gameBoard;
+    }
+
+    public Set<Point> getTargetCellsPositions() {
+        return targetCellsPositions;
+    }
+
+    public Point getPlayerPosition() {
+        return playerPosition;
+    }
+
+    public int getCurrentTargetCellsCompleted() {
+        return currentTargetCellsCompleted;
+    }
+
+    public void setCurrentTargetCellsCompleted(int currentTargetCellsCompleted) {
+        this.currentTargetCellsCompleted = currentTargetCellsCompleted;
+    }
+
+    public String toString(Point playerPosition) {
+        StringBuilder boardToString = new StringBuilder();
+        int row = 0;
+        int col = 0;
+        for (List<Tile> boardRow : gameBoard) {
+            for (Tile tile : boardRow) {
+                switch (tile) {
+                    case FLOOR:
+                        Point tilePosition = new Point(row, col);
+                        if (playerPosition.equals(tilePosition)) {
+                            boardToString.append('@');
+                        } else if (targetCellsPositions.contains(tilePosition)) {
+                            boardToString.append('.');
+                        } else {
+                            boardToString.append(' ');
+                        }
+                        break;
+                    case WALL:
+                        boardToString.append('#');
+                        break;
+                    case BOX:
+                        boardToString.append('$');
+                        break;
+                }
+                col += 1;
+            }
+            row += 1;
+            col = 0;
+            boardToString.append('\n');
+        }
+
+        return boardToString.toString();
+    }
+
+    private Tile processTileFromChar(int row, int col, Character character) {
+        switch (character) {
+            case '#':
+                return Tile.WALL;
+            case '.':
+                targetCellsPositions.add(new Point(row, col));
+                return Tile.FLOOR;
+            case '$':
+                return Tile.BOX;
+            case '@':
+                playerPosition = new Point(row, col);
+                return Tile.FLOOR;
+        }
+
+        return Tile.FLOOR;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        GameBoard gameBoard1 = (GameBoard) o;
+        return currentTargetCellsCompleted == gameBoard1.currentTargetCellsCompleted &&
+                gameBoard.equals(gameBoard1.gameBoard) &&
+                targetCellsPositions.equals(gameBoard1.targetCellsPositions) &&
+                playerPosition.equals(gameBoard1.playerPosition);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(gameBoard, targetCellsPositions, currentTargetCellsCompleted, playerPosition);
+    }
+}
--- src/GameState.java
+++ src/GameState.java
@@ -1,110 +1,47 @@
 import java.awt.Point;
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
 
 public class GameState {
-    private List<List<Tile>> gameBoard;
+    private final GameBoard gameBoard;
     private Point playerPosition;
-    private Integer currentTargetCellsCompleted;
-    private Set<Point> targetCellsPositions;
-    private Boolean stuckBox;
+    private boolean stuckBox;
 
-    public GameState(String configurationFilepath) throws IOException {
-        gameBoard = new ArrayList<List<Tile>>();
-        currentTargetCellsCompleted = 0;
-        targetCellsPositions = new HashSet<>();
+    public GameState(GameBoard gameBoard) {
+        this.gameBoard = gameBoard;
         stuckBox = false;
-        int row = 0;
-        int col = 0;
-
-        File configurationFile = new File(configurationFilepath);
-        BufferedReader br = new BufferedReader(new FileReader(configurationFile));
-        String line;
-        while ((line = br.readLine()) != null) {
-            gameBoard.add(new ArrayList<>());
-            for (Character character : line.toCharArray()) {
-                gameBoard.get(row).add(processTileFromChar(row, col, character));
-                col += 1;
-            }
-            row += 1;
-            col = 0;
-        }
     }
 
-    public GameState() {
-        playerPosition = new Point(0,0);
-    }
-
-    public void movePlayer(Direction moveDirection) {
+    public Point movePlayer(Direction moveDirection) {
         Point targetPosition = getTargetPositionFromDirection(moveDirection, playerPosition);
         System.out.println(targetPosition);
-        Tile targetTile = gameBoard.get(targetPosition.x).get(targetPosition.y);
+        Tile targetTile = gameBoard.getGameBoard().get(targetPosition.x).get(targetPosition.y);
         System.out.println(targetTile);
         switch (targetTile) {
             case FLOOR:
                 playerPosition = targetPosition;
                 break;
             case WALL:
-                //cant move
+                //can't move
                 break;
             case BOX:
                 if (canMoveBox(targetPosition, moveDirection)) {
                     playerPosition = targetPosition;
                     moveBox(targetPosition, moveDirection);
                 } else {
-                    System.out.println("Cant move box");
+                    System.out.println("Can't move box");
                 }
         }
 
-        System.out.println(currentTargetCellsCompleted);
+        System.out.println(gameBoard.getCurrentTargetCellsCompleted());
+
+        return playerPosition;
     }
 
-    public Boolean isGameWon() {
-        return currentTargetCellsCompleted.equals(targetCellsPositions.size());
+    public boolean isGameWon() {
+        return gameBoard.getCurrentTargetCellsCompleted() == gameBoard.getTargetCellsPositions().size();
     }
 
-    public Boolean isGameStuck() {
+    public boolean isGameStuck() {
         return stuckBox;
-    }
-
-    public String toString() {
-        StringBuilder boardToString = new StringBuilder();
-        int row = 0;
-        int col = 0;
-        for (List<Tile> boardRow : gameBoard) {
-            for (Tile tile : boardRow) {
-                switch (tile) {
-                    case FLOOR:
-                        Point tilePosition = new Point(row, col);
-                        if (playerPosition.equals(tilePosition)) {
-                            boardToString.append('@');
-                        } else if (targetCellsPositions.contains(tilePosition)) {
-                            boardToString.append('.');
-                        } else {
-                            boardToString.append(' ');
-                        }
-                        break;
-                    case WALL:
-                        boardToString.append('#');
-                        break;
-                    case BOX:
-                        boardToString.append('$');
-                        break;
-                }
-                col += 1;
-            }
-            row += 1;
-            col = 0;
-            boardToString.append('\n');
-        }
-
-        return boardToString.toString();
     }
 
     private Point getTargetPositionFromDirection(Direction moveDirection, Point currentPosition) {
@@ -129,23 +66,24 @@ public class GameState {
         return targetPosition;
     }
 
-    private Boolean canMoveBox(Point boxPosition, Direction moveDirection) {
+    private boolean canMoveBox(Point boxPosition, Direction moveDirection) {
         Point targetPosition = getTargetPositionFromDirection(moveDirection, boxPosition);
-        Tile targetTile = gameBoard.get(targetPosition.x).get(targetPosition.y);
+        Tile targetTile = gameBoard.getGameBoard().get(targetPosition.x).get(targetPosition.y);
         return targetTile == Tile.FLOOR;
     }
 
     private void moveBox(Point boxPosition, Direction moveDirection) {
+        int currentTargetCellsCompleted = gameBoard.getCurrentTargetCellsCompleted();
         Point targetPosition = getTargetPositionFromDirection(moveDirection, boxPosition);
-        gameBoard.get(boxPosition.x).set(boxPosition.y, Tile.FLOOR);
-        gameBoard.get(targetPosition.x).set(targetPosition.y, Tile.BOX);
+        gameBoard.getGameBoard().get(boxPosition.x).set(boxPosition.y, Tile.FLOOR);
+        gameBoard.getGameBoard().get(targetPosition.x).set(targetPosition.y, Tile.BOX);
 
-        if (targetCellsPositions.contains(boxPosition)) {
-            currentTargetCellsCompleted -= 1;
+        if (gameBoard.getTargetCellsPositions().contains(boxPosition)) {
+            gameBoard.setCurrentTargetCellsCompleted(currentTargetCellsCompleted - 1);
         }
 
-        if (targetCellsPositions.contains(targetPosition)) {
-            currentTargetCellsCompleted += 1;
+        if (gameBoard.getTargetCellsPositions().contains(targetPosition)) {
+            gameBoard.setCurrentTargetCellsCompleted(currentTargetCellsCompleted + 1);
         }
 
         if (isBoxBlocked(targetPosition)) {
@@ -153,27 +91,10 @@ public class GameState {
         }
     }
 
-    private Tile processTileFromChar(int row, int col, Character character) {
-        switch (character) {
-            case '#':
-                return Tile.WALL;
-            case '.':
-                targetCellsPositions.add(new Point(row, col));
-                return Tile.FLOOR;
-            case '$':
-                return Tile.BOX;
-            case '@':
-                playerPosition = new Point(row, col);
-                return Tile.FLOOR;
-        }
-
-        return Tile.FLOOR;
-    }
-
-    private Boolean isBoxBlocked(Point boxPosition) {
-        return !((gameBoard.get(boxPosition.x).get(boxPosition.y + 1) == Tile.FLOOR
-                && gameBoard.get(boxPosition.x).get(boxPosition.y - 1) == Tile.FLOOR)
-                || ((gameBoard.get(boxPosition.x + 1).get(boxPosition.y) == Tile.FLOOR)
-                && gameBoard.get(boxPosition.x - 1).get(boxPosition.y) == Tile.FLOOR));
+    private boolean isBoxBlocked(Point boxPosition) {
+        return !((gameBoard.getGameBoard().get(boxPosition.x).get(boxPosition.y + 1) == Tile.FLOOR
+                && gameBoard.getGameBoard().get(boxPosition.x).get(boxPosition.y - 1) == Tile.FLOOR)
+                || ((gameBoard.getGameBoard().get(boxPosition.x + 1).get(boxPosition.y) == Tile.FLOOR)
+                && gameBoard.getGameBoard().get(boxPosition.x - 1).get(boxPosition.y) == Tile.FLOOR));
     }
 }
